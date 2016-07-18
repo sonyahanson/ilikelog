724368a59c43ac30b546f22fb1ed6d040377e5f6
sonyahanson <hansons@mskcc.org>
2016-07-17 23:03:11 -0400

    initial commit sentence maker

---
+++ Ilikelog.py
@@ -0,0 +1,4 @@
+# Place holder will be the best script ever made.
+################ I Like Log ###################
+# The Open Source Automatic Logline Generator #
+###############################################
